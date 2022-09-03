2938fce0f20359e8bfe60ea019d34bea1b6970a7
ebin <ebinyesudas234@gmail.com>
2022-09-03 16:08:35 +0530

    admin can add product and view all products

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,10 +1,19 @@
 var db = require('../config/connection')
+var collection = require('../config/collections')
+const async = require('hbs/lib/async')
 module.exports = {
     addproduct : (product,callback)=>{
         console.log(product)
         db.get().collection('product').insertOne(product).then((data)=>{
             console.log(data.insertedId)
             callback(data.insertedId)
+        })
+    },
+    getAllproducts : ()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products =await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            //console.log(products)
+            resolve(products)
         })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,10 @@ var productHelpers = require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  const product=[{
-    name:"iphone 12",
-    catagory:"mobile",
-    description:"this is a good phone"
-
-  }]
-  res.render('admin/view-products',{product ,admin:true})
+  productHelpers.getAllproducts().then((products)=>{
+    console.log(products)
+    res.render('admin/view-products',{products ,admin:true})
+  })
 });
 
 router.get('/add-product',(req,res)=>{
