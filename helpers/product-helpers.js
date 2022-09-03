9cb40e1c4037372d3ea542552dd654f6105c69e3
ebin <ebinyesudas234@gmail.com>
2022-09-03 15:44:11 +0530

    product details added to the database

---
+++ helpers/product-helpers.js
@@ -0,0 +1,10 @@
+var db = require('../config/connection')
+module.exports = {
+    addproduct : (product,callback)=>{
+        console.log(product)
+        db.get().collection('product').insertOne(product).then((data)=>{
+            console.log(data.insertedId)
+            callback(data.insertedId)
+        })
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var productHelpers = require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -17,7 +18,19 @@ router.get('/add-product',(req,res)=>{
 })
 
 router.post('/add-product',(req,res)=>{
-  console.log(req.body)
-  console.log(req.files.image)
+  //console.log(req.body)
+  //console.log(req.files.image)
+  productHelpers.addproduct(req.body,(insertedId)=>{
+   // console.log(insertedId)
+   let image = req.files.image
+   image.mv('./public/product-images/'+insertedId+'.jpg',(err,done)=>{
+    if(!err){
+      res.render('admin/add-product')
+    }else{
+      console.log(err)
+    }
+   })
+    
+  })
 })
 module.exports = router;
