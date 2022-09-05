fc2ffb33bb2692bf97a3edaf99eaa0d172bfe3fd
ebin <ebinyesudas234@gmail.com>
2022-09-05 13:05:40 +0530

    admin side edit feature added

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -3,6 +3,7 @@ var collection = require('../config/collections')
 var objectId = require('mongodb').ObjectId
 const async = require('hbs/lib/async')
 const { response } = require('../app')
+const { ObjectId } = require('mongodb')
 module.exports = {
     addproduct : (product,callback)=>{
         console.log(product)
@@ -23,6 +24,31 @@ module.exports = {
             db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:objectId(prodId)}).then((response)=>{
                 //console.log(response)
                 resolve(response)
+            })
+        })
+    },
+    getProductDetails : (prodId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:ObjectId(prodId)})
+            .then((response)=>{
+                //console.log(response)
+                resolve(response)
+            })
+            
+
+        })
+    },
+    updateProduct : (prodId,prodDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id:objectId(prodId)},
+            {$set:{Name : prodDetails.Name,
+                   Catagory : prodDetails.Catagory,
+                   Price : prodDetails.Price,
+                   Description : prodDetails.Description}
+            }).then(()=>{
+                //console.log(response)
+                resolve()
             })
         })
     }
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const async = require('hbs/lib/async');
 var router = express.Router();
 var productHelpers = require('../helpers/product-helpers')
 
@@ -37,6 +38,24 @@ router.get('/delete-product/:id',(req,res)=>{
   productHelpers.deleteProduct(prodId).then((response)=>{
     //console.log(response)
     res.redirect('/admin')
+  })
+})
+
+router.get('/edit-product/:id',async(req,res)=>{
+  prodId = req.params.id
+  let product = await productHelpers.getProductDetails(prodId)
+  //console.log(product)
+  res.render('admin/edit-product',{product})
+})
+
+router.post('/edit-product/:id',(req,res)=>{
+  prodId = req.params.id
+  productHelpers.updateProduct(prodId,req.body).then(()=>{
+    res.redirect('/admin')
+    if(req.files.image){
+      let image = req.files.image
+      image.mv('./public/product-images/'+prodId+'.jpg')
+    }
   })
 })
 
