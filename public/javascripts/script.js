b937501bf6305f8d5bb2f4918ff39d97ef315aa6
ebin <ebinyesudas234@gmail.com>
2022-09-27 14:07:51 +0530

    sweetalert added to 'addtowishlist function'

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,5 @@
+// const { default: Swal } = require("sweetalert2")
+
 
 
 function addToCart(prodId){
@@ -22,11 +24,59 @@ function addToWishlist(prodId){
         method:'get',
         success:(response)=>{
             if(response.status){
+                Swal.fire({
+                    position:'top-center',
+                    icon:'success',
+                    title:'Item has been Added',
+                    showConfirmButton:false,
+                    timer:1000
+                })
                 let count = $('#wish_count').html()
                 console.log(count)
                 count = parseInt(count)+1
                 $('#wish_count').html(count)
+            }else{
+                Swal.fire({
+                    icon:'warning',
+                    text:'Already Exist in Your Wishlist'
+                })
             }
         }
     })
+}
+
+function removeWishlistProduct(prodId,wishlistId){
+        
+    Swal.fire({
+        title:'Are you sure?',
+        text:"You won't be able to revert this!",
+        width: 700,
+        
+        icon:'warning',
+        showCancelButton:true,
+        confirmButtonColor:'#3085d6',
+        cancelButtonColor:'#d33',
+        confirmButtonText:'Yes,delete it!'
+    }).then((result)=>{
+        if(result.isConfirmed){
+            $.ajax({
+        url:'/remove-wishlist-product',
+        data:{
+            prodId:prodId,
+            wishlistId:wishlistId
+        },
+        method:'post',
+        success:(response)=>{
+            if(response.removeProduct){
+                Swal.fire('Deleted!','Your file has been deleted.','success')
+               .then(()=>{
+               
+               location.reload()
+               })
+            }
+        }
+    })
+        }
+    })
+    
 }
